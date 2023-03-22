133aaab136bdd44e9845a88b3d5fe6f40c27f3fa
Marija Rubil <rubilmarija@gmail.com>
2023-03-22 22:57:37 +0100

    Exercise 03

--- exercises/03-roman-numerals/roman-numerals.js
+++ exercises/03-roman-numerals/roman-numerals.js
@@ -3,6 +3,30 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRoman = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+export function toRoman(num) {
+  const romans = [
+    { value: 1000, symbol: "M" },
+    { value: 900, symbol: "CM" },
+    { value: 500, symbol: "D" },
+    { value: 400, symbol: "CD" },
+    { value: 100, symbol: "C" },
+    { value: 90, symbol: "XC" },
+    { value: 50, symbol: "L" },
+    { value: 40, symbol: "XL" },
+    { value: 10, symbol: "X" },
+    { value: 9, symbol: "IX" },
+    { value: 5, symbol: "V" },
+    { value: 4, symbol: "IV" },
+    { value: 1, symbol: "I" },
+  ];
+
+  let result = "";
+
+  for (const pair of romans) {
+    while (num >= pair.value) {
+      result += pair.symbol;
+      num -= pair.value;
+    }
+  }
+  return result;
+}
